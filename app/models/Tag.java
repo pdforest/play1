01017c112e7aa2b8f64f74747798a7afa29390f6
pdforest <45498338+pdforest@users.noreply.github.com>
2020-05-27 16:23:20 -0300

    Agregue captcha y tags

---
+++ app/models/Tag.java
@@ -0,0 +1,45 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+ 
+import play.db.jpa.*;
+ 
+@Entity
+public class Tag extends Model implements Comparable<Tag> {
+ 
+    public String name;
+ 
+    private Tag(String name) {
+        this.name = name;
+    }
+ 
+    public String toString() {
+        return name;
+    }
+ 
+    public int compareTo(Tag otherTag) {
+        return name.compareTo(otherTag.name);
+    }
+    
+    public static Tag findOrCreateByName(String name) {
+        Tag tag = Tag.find("byName", name).first();
+        if(tag == null) {
+            tag = new Tag(name);
+        }
+        return tag;
+    }
+    
+    public static List<Map> getCloud() {
+        List<Map> result = Tag.find(
+            "select new map(t.name as tag, count(p.id) as zpound) from Post p left join p.tags as t group by t.name order by t.name"
+        ).fetch();
+
+//        for(Map l : result) {
+//        	System.out.println(l);
+//        }
+
+        return result;
+    }
+    
+}
--- test/BasicTest.java
+++ test/BasicTest.java
@@ -165,4 +165,39 @@ public class BasicTest extends UnitTest {
 	    assertEquals(4, Comment.count());
 	}
 	
+	@Test
+	public void testTags() {
+	    // Create a new user and save it
+	    User bob = new User("bob@gmail.com", "secret", "Bob").save();
+	 
+	    // Create a new post
+	    Post bobPost = new Post(bob, "My first post", "Hello world").save();
+	    Post anotherBobPost = new Post(bob, "Hop", "Hello world").save();
+	 
+	    // Well
+	    assertEquals(0, Post.findTaggedWith("Red").size());
+	 
+	    // Tag it now
+	    bobPost.tagItWith("Red").tagItWith("Blue").save();
+	    anotherBobPost.tagItWith("Red").tagItWith("Green").save();
+	 
+	    // Check
+	    assertEquals(2, Post.findTaggedWith("Red").size());
+	    assertEquals(1, Post.findTaggedWith("Blue").size());
+	    assertEquals(1, Post.findTaggedWith("Green").size());
+	    
+	    // Check combinado
+	    assertEquals(1, Post.findTaggedWith("Red", "Blue").size());
+	    assertEquals(1, Post.findTaggedWith("Red", "Green").size());
+	    assertEquals(0, Post.findTaggedWith("Red", "Green", "Blue").size());
+	    assertEquals(0, Post.findTaggedWith("Green", "Blue").size());
+	    
+	    // Check Tag Cloud
+	    List<Map> cloud = Tag.getCloud();
+	    assertEquals(
+	        "[{tag=Blue, zpound=1}, {tag=Green, zpound=1}, {tag=Red, zpound=2}]",
+	        cloud.toString()
+	    );
+	}
+	
 }
